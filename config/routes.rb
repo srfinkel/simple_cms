a9b49ba7a249b186dcaccab9a205409d43f56e94
Saru <srfinkel@gmail.com>
2019-07-04 07:14:55 -0600

    practice with instance variables and params

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
+  
   get 'home/about'
   get 'demo/index'
   get 'demo/hello'
   get 'demo/other_hello'
+
+  # default route 
+  # may be deprecated later on
+  # get ':controller(/:action(/:id))'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
