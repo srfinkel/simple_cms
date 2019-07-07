aec67db2549c0b53344d26fecc0bf4112b74c866
Saru <srfinkel@gmail.com>
2019-07-06 22:24:17 -0600

    ran migration for Pages

--- db/migrate/20190707040854_create_pages.rb
+++ db/migrate/20190707040854_create_pages.rb
@@ -1,8 +1,20 @@
 class CreatePages < ActiveRecord::Migration[5.2]
-  def change
+  
+  def up
     create_table :pages do |t|
 
+      t.integer "subject_id"
+      t.string "name"
+      t.integer "permalink"
+      t.integer "position"
+      t.boolean "visible"
+      
       t.timestamps
     end
   end
+
+  def down
+    drop_table :pages
+  end
+
 end
